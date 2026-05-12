aefee3ae5b66c4ac6a025a0e55787c4f58f12fb0
Attila <szt3daratt@vasvari.org>
2026-05-12 12:30:36 +0200

    A tippelések AI(ellenség) oldalról

---
+++ Kliensoldali_projekt/JS/szkript.js
@@ -0,0 +1,24 @@
+//A tippelések AI oldalról
+function gepTippelese() {
+    var veletlenSor = Math.floor(10 * Math.random());
+    var veletlenOszlop = Math.floor(10 * Math.random());
+    var jID = "j" + veletlenSor + veletlenOszlop;
+
+    if (!gepCelzott.includes(jID)) {
+        for (let i = 0; i < j1Hajok.length; i++) {
+            if (
+                j1Hajok[i].hely.includes(jID) &&
+                !j1Hajok[i].talalatok.includes(jID)
+            ) {
+                j1Hajok[i].talalatok.push(jID);
+                document.getElementById(jID).classList.remove("elhelyezve");
+                document.getElementById(jID).classList.add("talalat");
+                gepCelzott.push(jID);
+                gepTalalatSzam++;
+            } else if (!j1Hajok[i].hely.includes(jID)) {
+                document.getElementById(jID).classList.add("melle");
+                gepCelzott.push(jID);
+            }
+        }
+    }
+}
